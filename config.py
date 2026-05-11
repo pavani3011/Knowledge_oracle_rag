d3d97824224bb0b050fa4ddca8a800d606d09673
pavani <shinupavani3@gmail.com>
2026-05-11 15:00:55 +0530

    configuration file

---
+++ config.py
@@ -0,0 +1,13 @@
+DOCS_DIR = "./docs"
+CHROMA_DIR = "./chroma_db"
+COLLECTION_NAME ="knowledge_base"
+
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+
+RETRIEVER_K = 4
+RETRIEVER_FETCH_K = 20
+
+LLM_MODEL = "gpt-4o-mini"
+EMBEDDING_MODEL = "text-embedding-3-small"
+TEMPERATURE = 0.0
